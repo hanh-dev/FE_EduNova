21daa56c2fdde40b0f39ce02a7936f641429e6df
Hanh_Dev Clean <hanh.ho26@student.passerellesnumeriques.org>
2025-05-08 20:56:38 +0700

    SPGTS-48 feat: add toastify for login

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './services/providers/AuthContext';
 import AppRoutes from './routes/AppRoutes';
+import { ToastContainer } from 'react-toastify';
 function App() {
   return (
         <AuthProvider>
           <BrowserRouter>
             <AppRoutes />
+            <ToastContainer />
           </BrowserRouter>
         </AuthProvider>
   );
