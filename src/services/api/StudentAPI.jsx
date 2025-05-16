d213ed642a4518eeb19c09c7a6b290ff4bee55fd
KimThanh1801 <hokimthanh1234@gmail.com>
2025-05-17 00:51:25 +0700

    WIP: save

--- src/services/api/StudentAPI.jsx
+++ src/services/api/StudentAPI.jsx
@@ -1,40 +1,9 @@
 import { api } from "../../utils/constants";
-
-export const login = (credentials) => api.post("/login", credentials);
-export const logout = () => api.post("/logout");
-export const profile = () => api.get("/profile");
 export const getCourses = () => api.get("/student/courses");
 export const getFeedbacks = () => api.get("/student/feedbacks");
 export const login = (credentials) => api.post("/v1/login", credentials);
 export const logout = () => api.post("/v1/logout");
 export const profile = () => api.get('/profile');
-
-
-// const login = async (credentials) => {
-//     try {
-//         const response = await api.post("/login", credentials);
-//         const {access_token, refresh_token, user} = response.data;
-//         saveUser(user, access_token, refresh_token);
-//         setToken(access_token);
-//         setRefreshToken(refresh_token);
-//         return {user.username, role, access_token}
-        
-//     } catch (error) {
-//         console.error("Erorr login!", error);
-//     }
-// }
-
-// const logout = async() => {
-//     try {
-//         localStorage.removeItem('access_token');
-//         localStorage.removeItem('refresh_token');
-//         localStorage.removeItem('user');
-//     } catch (error) {
-//         console.error("Erorr at logout!", error);
-//     }
-// }
-
-// export {login, logout}
 
 const getClasses = async() => {
     try {
